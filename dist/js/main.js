5c6bb1e36f55ec4707645b887bc9cccc7b7dd879
AbramovNik <abr.nik96@gmail.com>
2019-12-09 19:12:21 +0200

    some-changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,8 +11,50 @@ $(document).on('click', '#publicate', function () {
 });
 
 $(document).on('click', '.delete', function () {
-    deletePost();
+    $(this).parents('.news').remove();
 });
+
+$(document).on('click', '.edit', function () {
+    const title = $(this).parents('.news').find('.title').text();
+    const content = $(this).parents('.news').find('.content').text();
+    $(this).parents('.news').append(
+        `<div class = 'edit-modal'>
+        <div class = 'modal-header'>
+        <div class  = 'btn-wrapp'><button class='close-modal'>X</button></div>
+        <strong>Title</strong>
+        <input class = 'input-edit' value = ' ${title}'>
+        </div>
+        <div class = 'modal-body'>
+        <strong>Content</strong>
+        <textarea class = 'textarea-edit' cols='30' rows= '10'>${content}</textarea>
+        </div>
+        <div class = 'btn-wrapp'>
+        <button class = 'edit-post'>Edit</button>
+        </div>
+        </div>`
+        
+    );
+
+    const editPost = function(){
+        const editTitle = $('.input-edit').val();
+        const editContent = $('.textarea-edit').val();
+        $(this).parents('.news').find('.title').text(editTitle);
+        $(this).parents('.news').find('.content').text(editContent);
+        $(this).parents('.news').find('.edit-modal').remove();
+    
+    }
+
+    const closeModal = function(){
+        $(this).parents('.news').find('.edit-modal').remove();
+    }
+
+    $('.close-modal').on('click', closeModal);
+
+    $('.edit-post').on('click', editPost);
+    
+});
+
+
 
 const openModal = function () {
     $('#openModal').addClass('modal-open');
@@ -22,6 +64,8 @@ const closeModal = function () {
     $('#openModal').removeClass('modal-open');
     $('#title').val('');
     $('#content').val('');
+
+    
 }
 
 
@@ -40,25 +84,23 @@ function checkPost() {
 const publicatePost = function () {
     const title = $('#title').val();
     const content = $('#content').val();
-    console.log(title, content);
     $('#openModal').removeClass('modal-open');
     $('.news-list').append(`<div class = 'news'>
+    <div class = 'content-wrapp'>
  <strong class = 'title'>${title}</strong>
+ <p class = 'content'>${content}</p>
+ </div>
  <div class = btn-wrapp>
  <button class = 'edit'>edit</button>
  <button class = 'delete'>delete</button>
  </div>
- <p class = 'content'>${content}</p>
  </div>`);
  $('#title').val('');
  $('#content').val('');
 }
 
-const deletePost = function(){
-console.log($(this));
-console.log("dsdasas");
-console.log("sdjaslasjk")
-}
+
+
 $(document).on('click', '#btnSubmit', function (e) {
     e.preventDefault();
     e.stopPropagation();
